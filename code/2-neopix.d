2ee7d396b0ebbd43213a501af74883bfd16242ea
Avi Udash <udashavi@gmail.com>
2021-11-22 21:38:54 -0800

    add gitignore

--- code/2-neopix.d
+++
@@ -1,6 +0,0 @@
-2-neopix.o: 2-neopix.c neopixel.h \
- /home/avi/School/Fall21/CS49N/cs49n-21aut/libpi/include/rpi.h \
- /home/avi/School/Fall21/CS49N/cs49n-21aut/libpi/include/gpio.h \
- /home/avi/School/Fall21/CS49N/cs49n-21aut/libpi/include/../libc/libc-extra.h \
- /home/avi/School/Fall21/CS49N/cs49n-21aut/libpi/include/../libc/assert.h \
- /home/avi/School/Fall21/CS49N/cs49n-21aut/libpi/include/rpi-constants.h
